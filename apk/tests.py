74a4b48c8606c158ff3a8be3a035118198b3d4cd
Cheng-Lung Sung <clsung@gmail.com>
2014-03-25 17:11:59 +0800

    [test] test upload

---
+++ apk/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.test.client import Client
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.urlresolvers import reverse
+
+
+class UploadAPKTests(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def tearDown(self):
+        pass
+
+    def test_upload_single_apk(self):
+        url = reverse('upload')
+        f = SimpleUploadedFile("apk1_1.0.0.apk", "file_content")
+        response = self.c.post(
+            url,
+            {
+                "package_name": "apk1",
+                "version": "1.0.0",
+                "activity_intent": "activity",
+                "service_intent": "service",
+                "reverse_intent": "receiver",
+                "file": f,
+            }
+        )
+        self.assertEqual(response.status_code, 201)
